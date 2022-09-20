42ea06713a8fde0d7afe71415b4ac0b1c10d46dd
JasonDisj <jasondisj@gmail.com>
2022-09-20 00:24:30 +0000

    Add validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
 
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :has_no_apparel
+
+  def has_no_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add("Is this a store?!")
+    end
+  end
 end
